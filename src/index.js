62a3383c94b1e0daac6b088317f4bdfb3be656b8
Robert Bergers <bergersr@my.easternct.edu>
2019-06-15 15:25:08 -0400

    properly configure paths, set handlebars

--- src/index.js
+++ src/index.js
@@ -6,12 +6,16 @@ const hbs = require('hbs')
 // Connect local files and db
 require('./db/mongoose')
 const appRouters = require('./routers/app')
-const publicPages = path.join('../public')
+const publicPages = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
 
 // Express config
 const port = 3000
 const app = express()
+app.use(express.static(publicPages))
 app.use(express.json())
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
 app.use(appRouters)
 
 // Run server
