3befcf1b9cdd7b84dbc0b39c3583bdc60c2aa180
Robert Bergers <bergersr@my.easternct.edu>
2019-06-15 15:25:20 -0400

    homepage routes to portfolio

--- src/routers/app.js
+++ src/routers/app.js
@@ -1,9 +1,16 @@
+// Libraries
 const express = require('express')
 const path = require('path')
-const publicPages = path.join('../../public')
-
+const hbs = require('hbs')
 const router = new express.Router()
 
+// Routes
 router.get('', (req, res) => {
-  res.send(publicPages + '/portfolio1')
+  try {
+    res.render('portfolio/index')
+  } catch (e) {
+    res.send()
+  }
 })
+
+module.exports = router
