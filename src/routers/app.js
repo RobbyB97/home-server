7a6d0a34412824aa3f6b3b66136bc6404dbdc7c5
Robert Bergers <bergersr@my.easternct.edu>
2019-06-17 14:47:07 -0400

    import and include middleware func

--- src/routers/app.js
+++ src/routers/app.js
@@ -2,10 +2,13 @@
 const express = require('express')
 const path = require('path')
 const hbs = require('hbs')
+
+// Config
 const router = new express.Router()
+const http2https = require('../middleware/http2https')
 
 // Routes
-router.get('', (req, res) => {
+router.get('', http2https, (req, res) => {
   try {
     res.render('portfolio/index')
   } catch (e) {
