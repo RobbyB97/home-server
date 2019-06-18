0616573c3fc442ed0fbbb4dccced22520b3fc80f
Robert Bergers <bergersr@my.easternct.edu>
2019-06-18 14:47:03 -0400

    refactor, add weather router

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ const fs = require('fs')
 
 // Connect local files and db
 //require('./db/mongoose')
-const appRouters = require('./routers/app')
 const publicPages = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -24,7 +23,12 @@ app.use(express.static(publicPages))
 app.use(express.json())
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
-app.use(appRouters)
+
+// Routers
+const mainRouter = require('./routers/app')
+const weatherRouter = require('./routers/weather')
+app.use(mainRouter)
+app.use(weatherRouter)
 
 hbs.registerPartials(partialsPath)
 
