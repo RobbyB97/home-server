d501f676f25b13a506791d3d121b8b2a61a4c565
Robert Bergers <bergersr@my.easternct.edu>
2019-06-17 23:24:03 -0400

    add weather app home route

--- src/routers/app.js
+++ src/routers/app.js
@@ -11,7 +11,18 @@ const http2https = require('../middleware/http2https')
 router.get('', http2https, (req, res) => {
   try {
     res.render('portfolio/index')
-  } catch (e) {
+  } catch(e) {
+    res.send()
+  }
+})
+
+router.get('/weather', http2https, (req, res) => {
+  try {
+    res.render('weather/index', {
+      title: 'Weather App',
+      name: 'Robby'
+    })
+  } catch(e) {
     res.send()
   }
 })
