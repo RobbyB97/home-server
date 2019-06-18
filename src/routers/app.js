69af81e860d28d375e0cca7d80ab7edcb86f5173
Robert Bergers <bergersr@my.easternct.edu>
2019-06-18 14:46:34 -0400

    router only used for homepage / portfolio

--- src/routers/app.js
+++ src/routers/app.js
@@ -7,21 +7,10 @@ const hbs = require('hbs')
 const router = new express.Router()
 const http2https = require('../middleware/http2https')
 
-// Routes
+// Homepage (Portfolio)
 router.get('', http2https, (req, res) => {
   try {
     res.render('portfolio/index')
-  } catch(e) {
-    res.send()
-  }
-})
-
-router.get('/weather', http2https, (req, res) => {
-  try {
-    res.render('weather/index', {
-      title: 'Weather App',
-      name: 'Robby'
-    })
   } catch(e) {
     res.send()
   }
