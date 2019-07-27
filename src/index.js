f8ac8f21fa972da3b5aebc82379a6e2e1f7e1e0f
Robert Bergers <me@robertbergers.com>
2019-07-27 08:05:02 -0700

    Refactor to support production and dev environments

--- src/index.js
+++ src/index.js
@@ -3,43 +3,74 @@
 // Libraries
 const express = require('express')
 const https = require('https')
+const http = require('http')
 const path = require('path')
 const hbs = require('hbs')
 const fs = require('fs')
+const chalk = require('chalk')
 
-// Connect local files and db
+
+// Environment variables
+const port = process.env.PORT
+const dev = process.env.DEV
+console.log(chalk.blue('Environment Variables:\n'), chalk.green(`Port: ${port}\n Dev: ${dev}`))
+
+
+// (Uncomment when database in use)
 //require('./db/mongoose')
+
+
+// Connect local files
 const publicPages = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
-const key = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/privkey.pem')
-const cert = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/fullchain.pem')
-const ca = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/chain.pem')
+
+const mainRouter = require('./routers/app')
+const weatherRouter = require('./routers/weather')
+
 
 // Express config
-const port = 443
 const app = express()
 app.use(express.static(publicPages))
 app.use(express.json())
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
 
-// Routers
-const mainRouter = require('./routers/app')
-const weatherRouter = require('./routers/weather')
+
+// Server config
+if (dev == 1) {
+  console.log(chalk.green('Running server in dev environment'))
+} else {
+  console.log(chalk.green('Running server in prod environment'))
+  // Connect SSL Certs
+  const key = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/privkey.pem')
+  const cert = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/fullchain.pem')
+  const ca = fs.readFileSync('/etc/letsencrypt/live/bergers.dev/chain.pem')
+
+  // HTTPS Config
+  const httpsOptions = {
+    cert: cert,
+    key: key,
+    ca: ca
+  }
+}
+
+
+// Connect routers
 app.use(mainRouter)
 app.use(weatherRouter)
 
-hbs.registerPartials(partialsPath)
-
-// HTTPS Config
-const httpsOptions = {
-  cert: cert,
-  key: key,
-  ca: ca
-}
 
 // Run server
-https.createServer(httpsOptions, app).listen(443, () => {
-  console.log('HTTPS running... Port: 443')
-})
+if (dev == 1) {
+  // Dev
+  http.createServer(app).listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+  })
+} else {
+  // Production
+  https.createServer(httpsOptions, app).listen(443, () => {
+    console.log(`Production HTTPS server running on port: ${port}`)
+  })
+}
