d11c13a5cb8652e99dd0dab62054e6b27f26d649
Robert Bergers <me@robertbergers.com>
2019-07-27 08:13:25 -0700

    refactor to support dev and prod environments

--- src/middleware/http2https.js
+++ src/middleware/http2https.js
@@ -1,8 +1,23 @@
+// Libraries
+const chalk = require('chalk')
+
+
+// Environment variables
+const dev = process.env.DEV
+
+
+// Ensure secure connection
 const http2https = (req, res, next) => {
-  if(req.secure){
-    return next();
-  };
-  res.redirect('https://' + req.hostname + req.url);
+  if (dev == 1) {
+    console.log(chalk.blue('Skipping secure redirect on dev environment'))
+    return next()
+  } else {
+    if(req.secure){
+      return next()
+    };
+    console.log(chalk.blue('Redirecting to https...'))
+    res.redirect('https://' + req.hostname + req.url)
+  }
 }
 
 module.exports = http2https
