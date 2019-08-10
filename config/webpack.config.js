3fdcbd752f9e694efdf19e362965bf510b8e5546
Robert Bergers <me@robertbergers.com>
2019-08-10 06:26:47 -0700

    add initial react file

---
+++ config/webpack.config.js
@@ -0,0 +1,31 @@
+// Libraries
+const path = require('path')
+
+
+
+module.exports = {
+  entry: './src/react/app.js',
+  output: {
+    path: path.join(__dirname, 'public/react'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }, {
+      test: /\.s?css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public/react'),
+    historyApiFallback: true
+  }
+}
