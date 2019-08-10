97dedd933da1daaececd46881d0795f16951aa51
Robert Bergers <me@robertbergers.com>
2019-08-10 07:35:29 -0700

    Add BrowserRouter component

---
+++ src/react/routers/AppRouter.js
@@ -0,0 +1,23 @@
+//Libraries
+import React from 'react'
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+//Components
+import Header from '../components/Header'
+import ReactHome from '../components/ReactHome'
+import ReactTest1 from '../components/ReactTest1'
+
+// Routes
+const AppRouter = () => (
+  <BrowserRouter>
+    <div>
+      <Header/>
+      <Switch>
+        <Route path="/react/" component={ReactHome} exact={true}/>
+        <Route path="/react/test" component={ReactTest1} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+)
+
+export default AppRouter
