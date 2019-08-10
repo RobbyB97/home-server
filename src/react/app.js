a8ef47b1f1b94d226602d8c85c9c6f2877a37cd8
Robert Bergers <me@robertbergers.com>
2019-08-10 07:35:08 -0700

    Add root react file

--- src/react/app.js
+++ src/react/app.js
@@ -1,1 +1,10 @@
-console.log('app runnin!!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'normalize.css/normalize.css'
+import AppRouter from './routers/AppRouter'
+
+
+// Variables
+const $appRoot = document.getElementById('app')
+
+ReactDOM.render(<AppRouter />, $appRoot)
