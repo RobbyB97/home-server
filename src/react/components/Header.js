cef0f641bc0db4dfd38073be3703541c20488b8b
Robert Bergers <me@robertbergers.com>
2019-08-11 08:15:13 -0700

    add logo to Header component

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -4,6 +4,11 @@ import {NavLink} from 'react-router-dom'
 
 const Header = () => (
   <header>
+    <div id='head-logo'>
+      <a href='#'>
+        <img src='/assets/images/favicon.png' alt='Home'/> 
+      </a>
+    </div>
     <h1>Header</h1>
   </header>
 )
