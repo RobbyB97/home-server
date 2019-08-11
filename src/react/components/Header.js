8d17a93d90273d2cb5172b25bb2aa888fdd0dd1b
Robert Bergers <me@robertbergers.com>
2019-08-11 16:31:47 -0700

    add inter-nav button

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -2,13 +2,14 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
-const Header = () => (
+const Header = (props) => (
   <header id="head">
-    <div id='head-logo'>
-      <a href='#'>
+    <div id="head-logo">
+    <a href='#'>
         <img src='/assets/images/favicon.png' alt='Home'/> 
       </a>
     </div>
+    <button className="inter-nav"></button>
     <h1>Header</h1>
   </header>
 )
