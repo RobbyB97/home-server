3f002108035754f8a1431222e4f8ad4c3b1563c0
Robert Bergers <me@robertbergers.com>
2019-08-13 16:37:38 -0700

    Add NavLinks to Header

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -2,13 +2,17 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
 
+import StyleGuide from './StyleGuide'
+
 const Header = (props) => (
   <header id="head">
     <div id="head-logo">
-    <a href='#'>
+      <a href='#'>
         <img src='/assets/images/favicon.png' alt='Home'/> 
       </a>
     </div>
+    <NavLink to="/react/">Home</NavLink>
+    <NavLink to="/react/style_guide">Style Guide</NavLink>
     <button className="inter-nav"></button>
     <h1>Header</h1>
   </header>
