668465b5a22c9eb49247a63689c28e668d0d865b
Robert Bergers <me@robertbergers.com>
2019-08-15 19:02:38 -0700

    convert Header component into class

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -4,19 +4,38 @@ import {NavLink} from 'react-router-dom'
 
 import StyleGuide from './StyleGuide'
 
-const Header = (props) => (
-  <header id="head">
-    <div id="head-logo">
-      <a href='#'>
-        <img src='/assets/images/favicon.png' alt='Home'/> 
-      </a>
-    </div>
-    <NavLink to="/react/">Home</NavLink>
-    <NavLink to="/react/style_guide">Style Guide</NavLink>
-    <button className="inter-nav"></button>
-    <h1>Header</h1>
-  </header>
-)
+export default class Header extends React.Component {
+  state = {
+    active: this.props.active
+  }
 
+  toggle = () => {
+    console.log(this.props.active)
+    this.setState(() => ({
+      active: this.props.active ? true: false
+    }))
+    console.log(this.props.active)
+  }
 
-export default Header
+  render () {
+    return (
+      <header id="head">
+        <div id="head-logo">
+          <a href='#'>
+            <img src='/assets/images/favicon.png' alt='Home'/> 
+          </a>
+        </div>
+        <NavLink to="/react/">Home</NavLink>
+        <NavLink to="/react/style_guide">Style Guide</NavLink>
+        <button className="inter-nav" onClick={this.toggle}>
+          {this.props.active ? '✕':'☰'}
+        </button>
+        <h1>Header</h1>
+      </header>
+    )
+  }
+}
+
+Header.defaultProps = {
+  active: true
+}
