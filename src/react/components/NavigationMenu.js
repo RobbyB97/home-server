fed5beeab3ddca5aaf916a1c474443f7007aa0ee
Robert Bergers <me@robertbergers.com>
2019-08-17 07:30:51 -0700

    fix root id

--- src/react/components/NavigationMenu.js
+++ src/react/components/NavigationMenu.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 
 const NavigationMenu = (props) => (
     <div id="navigationMenu" className={props.activeNav ? 'active-nav':''}>
-        <div class="nav-bg">
+        <div id="nav-bg">
             <ul className="intra-nav">
                 <li><NavLink to="/react/" onClick={props.toggleNav}>Home</NavLink></li>
                 <li><NavLink to="/react/style_guide" onClick={props.toggleNav}>Style Guide</NavLink></li>
