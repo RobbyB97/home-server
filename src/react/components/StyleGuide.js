bd5892bcf943bdd64b35afd17a15bbd20a76521b
Robert Bergers <me@robertbergers.com>
2019-08-16 18:28:59 -0700

    refactor: convert StyleGuide component to class

--- src/react/components/StyleGuide.js
+++ src/react/components/StyleGuide.js
@@ -3,34 +3,57 @@ import Header from './Header'
 import ReactHome from './ReactHome'
 import ReactTest1 from './ReactTest1'
 
-const StyleGuide = () => (
-    <div class="main-content">
-        <h1>This is an h1</h1>
-        <h2>This is an h2</h2>
-        <h3>This is an h3</h3>
-        <h4>This is an h4</h4>
-        <h5>This is an h5</h5>
-        <h6>This is an h6</h6>
-        <br/>
-        <p>
-            This is a standard p tag,<br/>
-            <a href="#">This is a link</a>
-        </p>
-        <ul>
-            <li>This</li>
-            <li>is</li>
-            <li>an</li>
-            <li>unordered</li>
-            <li>list</li>
-        </ul>
-        <ol>
-            <li>This</li>
-            <li>is</li>
-            <li>an</li>
-            <li>ordered</li>
-            <li>list</li>
-        </ol>
-    </div>
-)
+export default class StyleGuide extends React.Component {
+    state = {
+        active: this.props.activeNav
+    }
 
-export default StyleGuide
+    toggleNav = () => {
+        let thing = !this.state.activeNav
+        this.setState(() => ({
+            activeNav: thing
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <Header 
+                    toggleNav={this.toggleNav}
+                    activeNav={this.state.activeNav}
+                />
+                <div class="main-content">
+                    <h1>This is an h1</h1>
+                    <h2>This is an h2</h2>
+                    <h3>This is an h3</h3>
+                    <h4>This is an h4</h4>
+                    <h5>This is an h5</h5>
+                    <h6>This is an h6</h6>
+                    <br/>
+                    <p>
+                        This is a standard p tag,<br/>
+                        <a href="#">This is a link</a>
+                    </p>
+                    <ul>
+                        <li>This</li>
+                        <li>is</li>
+                        <li>an</li>
+                        <li>unordered</li>
+                        <li>list</li>
+                    </ul>
+                    <ol>
+                        <li>This</li>
+                        <li>is</li>
+                        <li>an</li>
+                        <li>ordered</li>
+                        <li>list</li>
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+} 
+
+StyleGuide.defaultProps = {
+    activeNav: false,
+}
