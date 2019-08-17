c093e748210ddfba6cd53e73d094860478263170
Robert Bergers <me@robertbergers.com>
2019-08-16 18:55:51 -0700

    refactor: remove Header component

--- src/react/components/StyleGuide.js
+++ src/react/components/StyleGuide.js
@@ -17,38 +17,32 @@ export default class StyleGuide extends React.Component {
 
     render() {
         return (
-            <div>
-                <Header 
-                    toggleNav={this.toggleNav}
-                    activeNav={this.state.activeNav}
-                />
-                <div class="main-content">
-                    <h1>This is an h1</h1>
-                    <h2>This is an h2</h2>
-                    <h3>This is an h3</h3>
-                    <h4>This is an h4</h4>
-                    <h5>This is an h5</h5>
-                    <h6>This is an h6</h6>
-                    <br/>
-                    <p>
-                        This is a standard p tag,<br/>
-                        <a href="#">This is a link</a>
-                    </p>
-                    <ul>
-                        <li>This</li>
-                        <li>is</li>
-                        <li>an</li>
-                        <li>unordered</li>
-                        <li>list</li>
-                    </ul>
-                    <ol>
-                        <li>This</li>
-                        <li>is</li>
-                        <li>an</li>
-                        <li>ordered</li>
-                        <li>list</li>
-                    </ol>
-                </div>
+            <div className="main-content">
+                <h1>This is an h1</h1>
+                <h2>This is an h2</h2>
+                <h3>This is an h3</h3>
+                <h4>This is an h4</h4>
+                <h5>This is an h5</h5>
+                <h6>This is an h6</h6>
+                <br/>
+                <p>
+                    This is a standard p tag,<br/>
+                    <a href="#">This is a link</a>
+                </p>
+                <ul>
+                    <li>This</li>
+                    <li>is</li>
+                    <li>an</li>
+                    <li>unordered</li>
+                    <li>list</li>
+                </ul>
+                <ol>
+                    <li>This</li>
+                    <li>is</li>
+                    <li>an</li>
+                    <li>ordered</li>
+                    <li>list</li>
+                </ol>
             </div>
         )
     }
