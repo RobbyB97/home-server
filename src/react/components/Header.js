21de16dbb6c315ea02e2e5e137d356e436b772d8
Robert Bergers <me@robertbergers.com>
2019-08-18 18:25:54 -0700

    refactor: Header gets nav state from store

--- src/react/components/Header.js
+++ src/react/components/Header.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import {NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
 
+import toggleNav from '../actions/navigation'
 import StyleGuide from './StyleGuide'
 import NavigationMenu from './NavigationMenu'
 
-const Header = (props) => (
+const Header = ({nav, dispatch}) => (
   <header id="head">
     <div id="head-logo">
       <a href='#'>
         <img src='/assets/images/favicon.png' alt='Home'/> 
       </a>
     </div>
-    <button id="nav-button" onClick={props.toggleNav}>
-      {!props.activeNav ? '☰':'✕'}
+    <button id="nav-button" onClick={() => {
+      dispatch(toggleNav())
+    }}>
+      {!nav ? '☰':'✕'}
     </button>
-    <h1>Header</h1>
+    <h1>{!!nav}</h1>
   </header>
 ) 
 
-export default Header
+const mapStateToProps = (state) => {
+  return {
+    nav: state.navigation.nav
+  }
+}
+
+export default connect(mapStateToProps)(Header)
