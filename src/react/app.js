77ba4ffdafce3d438f22011b24f24abd9f4c90bc
Robert Bergers <me@robertbergers.com>
2019-08-19 16:55:37 -0700

    add setTitle function

--- src/react/app.js
+++ src/react/app.js
@@ -1,21 +1,28 @@
+// Libraries
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import 'normalize.css/normalize.css'
 
+
+// Assets
 import '../sass/main.scss'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
 import toggleNav from './actions/navigation'
+import {setTitle} from './actions/title'
+
 
 // Variables
 const $appRoot = document.getElementById('app')
+
 
 // Configure store
 const store = configureStore()
 store.subscribe(() => {
     console.log(store.getState())
 })
+
 
 const jsx = (
     <Provider store={store}>
