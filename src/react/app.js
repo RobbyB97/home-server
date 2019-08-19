91875d5819b662451869e064a3d15dc4f7b2342d
Robert Bergers <me@robertbergers.com>
2019-08-18 18:05:55 -0700

    connect redux store to project

--- src/react/app.js
+++ src/react/app.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
 import 'normalize.css/normalize.css'
 
 import '../sass/main.scss'
 import AppRouter from './routers/AppRouter'
 import configureStore from './store/configureStore'
+import toggleNav from './actions/navigation'
 
 // Variables
 const $appRoot = document.getElementById('app')
@@ -12,6 +14,14 @@ const $appRoot = document.getElementById('app')
 const store = configureStore()
 
 console.log(store.getState())
+store.dispatch(toggleNav())
+console.log(store.getState())
 
-ReactDOM.render(<AppRouter />, $appRoot)
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+)
+
+ReactDOM.render(jsx, $appRoot)
 
