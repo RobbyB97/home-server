03c143faa349be8b9c8735498a32be7b88b0e797
Robert Bergers <me@robertbergers.com>
2019-08-20 14:58:53 -0700

    add basic Article component

---
+++ src/react/components/Article.js
@@ -0,0 +1,26 @@
+/*
+   Standard Article
+*/
+
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+import {connect} from 'react-redux'
+
+const Article = (props) => (
+    <div className="article">
+        {props.title && 
+            <h2 className="article__title">
+                {props.title}
+            </h2>
+        }
+
+        {props.text &&
+            <p className="article__text">
+                {props.text}
+            </p>
+        }
+
+    </div>
+)
+
+export default connect()(Article)
