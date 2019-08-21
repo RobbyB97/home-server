dbe3b6aee579047752b63ab7edbe50bba234e656
Robert Bergers <me@robertbergers.com>
2019-08-20 19:18:24 -0700

    create wrap for homepage

--- src/react/components/pages/ReactApp.js
+++ src/react/components/pages/ReactApp.js
@@ -3,15 +3,17 @@ import React from 'react'
 import Header from '../Header'
 import StyleGuide from './StyleGuide'
 
-import {NavLink} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 
 export default class ReactApp extends React.Component {
 
     render() {
         return (
             <div>
-                <div>no!</div>
-                <p>ok</p>
+                <section className="space home-cover"></section>
+                <div className="home-cover">
+                    
+                </div>
             </div>
         )
     }
