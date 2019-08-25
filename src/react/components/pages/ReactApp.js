de7b616cbeaf0dd5eada79388c872bf0db58fea9
Robert Bergers <me@robertbergers.com>
2019-08-25 08:40:30 -0700

    add test Card to homepage

--- src/react/components/pages/ReactApp.js
+++ src/react/components/pages/ReactApp.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Header from '../Header'
 import StyleGuide from './StyleGuide'
+import Card from '../Card'
 
 import {Link, NavLink} from 'react-router-dom'
 
@@ -11,8 +12,19 @@ export default class ReactApp extends React.Component {
         return (
             <div>
                 <section className="space home-cover"></section>
+                <div className="home-text one">
+                        <h2>Home-text 1</h2>
+                    </div>
+                    <div className="home-text two">
+                        <h2>Home-text 2</h2>
+                    </div>
+                    <Card
+                        link="https://www.github.com/robbyb97"
+                        image="/assets/images/favicon.png"
+                        title="My Github"
+                        subtext="View my projects"
+                    />
                 <div className="home-cover">
-                    
                 </div>
             </div>
         )
