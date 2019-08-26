0086782ee0410e28cdc5b5dd58c42f495f07d88e
Robert Bergers <me@robertbergers.com>
2019-08-25 18:16:59 -0700

    add basic LogoBar component

---
+++ src/react/components/LogoBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const LogoBar = () => (
+    <div id="logo-bar">
+        <ul>
+            <li>
+                <a href="https://www.github.com/robbyb97">
+                    <img src="/assets/images/icons/github.svg" />
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/robbyb97">
+                    <img src="/assets/images/icons/linkedin.png" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default LogoBar
