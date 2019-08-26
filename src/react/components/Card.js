5f9184a00a5b35f1b1ae831121f6915aae5e598f
Robert Bergers <me@robertbergers.com>
2019-08-25 18:17:09 -0700

    add card wrapper

--- src/react/components/Card.js
+++ src/react/components/Card.js
@@ -2,11 +2,17 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 const Card = ({image, title, subtext, link}) => (
-    <div className="card">
-        <img src={image}/>
-        <div className="card__text">
-            <h2><Link to={link}>{title}</Link></h2>
-            <p>{subtext}</p>
+    <div>
+        <div className="card__wrap">
+            <a className="card__link" href={link}>
+                <div className="card">
+                    <img src={image}/>
+                    <div className="card__text">
+                        <h2>{title}</h2>
+                        <p>{subtext}</p>
+                    </div>
+                </div>
+            </a>
         </div>
     </div>
 )
