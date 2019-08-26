9987de9ca7b2640685e41abd0afa1f699288d9f2
Robert Bergers <me@robertbergers.com>
2019-08-26 16:27:07 -0700

    add homeToggle button to homePage

--- src/react/components/pages/ReactApp.js
+++ src/react/components/pages/ReactApp.js
@@ -1,24 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 import Header from '../Header'
 import StyleGuide from './StyleGuide'
 import Card from '../Card'
 
+import {toggleHomePage} from '../../actions/home'
+
 import {Link, NavLink} from 'react-router-dom'
 
-export default class ReactApp extends React.Component {
-
-    render() {
-        return (
+const ReactApp = ({dispatch, nav, title, home, homePage}) =>(
             <div>
-                <section className="space home-cover"></section>
+                
                 <div className="home-text one">
                         <h2>Home-text 1</h2>
+                        {homePage}
                     </div>
                     <div className="home-text two">
                         <h2>Home-text 2</h2>
                     </div>
-                <div className="home-cover">
+                <div id="home-cover" className={homePage ? "active":""}>
                     <Card
                         link="https://www.github.com/robbyb97"
                         image="/assets/images/favicon.png"
@@ -26,8 +27,23 @@ export default class ReactApp extends React.Component {
                         subtext="View my projects"
                     />
                 </div>
+                <div id="home-toggle" className={homePage ? "active":""}>
+                    <button onClick={() => {
+                        dispatch(toggleHomePage())
+                    }}>
+                        {homePage ? "▲":"▼"}
+                    </button>
+                </div>
             </div>
         )
-    }
 
-}
+const mapStateToProps = (state) => {
+    return {
+      nav: state.navigation.nav,
+      title: state.title.title,
+      home: state.home.home,
+      homePage: state.home.homePage
+    }
+  }
+
+  export default connect(mapStateToProps)(ReactApp)
