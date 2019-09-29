a28c6870354fefd50284a17de7f1836654d13299
Robert Bergers <me@robertbergers.com>
2019-09-29 12:55:40 -0700

    add SplashPage component skeleton

---
+++ src/react/components/SplashPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import { connect } from 'react-redux'
+
+const SplashPage = ({dispatch}) => {
+    <div id="splash-page">
+        <h2>
+            Splash Page main text
+        </h2>
+        <h3>
+            Splash Page subtext
+        </h3>
+        <button>
+            Splash Page button
+        </button>
+    </div>
+}
