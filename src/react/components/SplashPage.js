6890406fa3732ba806ca18d99e684aa7e115a045
Robert Bergers <bergersr@my.easternct.edu>
2019-11-17 19:59:46 -0500

    Bug fix: fix overflow-x

--- src/react/components/SplashPage.js
+++ src/react/components/SplashPage.js
@@ -7,13 +7,13 @@ import {splashOff} from '../actions/home'
 const SplashPage = ({splash, dispatch}) => (
     <div id="splash-page" className={splash ? "":"hidden"}>
         <div class="splash-content">
-            <h2>
+            <h2 className="text-one">
                 Designer. <br/> &nbsp; Developer. <br/> &nbsp;&nbsp; Problem Solver.
             </h2>
-            <h3>
+            <h3 className="text-two">
                 Robby Bergers
             </h3>
-            <button onClick={() => {
+            <button className="to-site" onClick={() => {
                 dispatch(splashOff())
             }}>
                 Splash Page button
