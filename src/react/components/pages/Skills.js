53979562a48c29c67b5942841c805b36f0d901c6
Robert Bergers <bergersr@my.easternct.edu>
2019-11-19 14:20:09 -0500

    Skills and Experience page skeletons

--- src/react/components/pages/Skills.js
+++ src/react/components/pages/Skills.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import {connect} from 'react-redux'
 
-import Header from '../Header'
-import StyleGuide from './StyleGuide'
-import Card from '../Card'
 
-import {toggleHomePage} from '../../actions/home'
 
-import {Link, NavLink} from 'react-router-dom'
+const Skills = ({dispatch, nav, title}) => (
+    <div>
+        Skills component
+    </div>
+)
+
 
 
 const mapStateToProps = (state) => {
     return {
-      nav: state.navigation.nav,
-      title: state.title.title,
-      home: state.home.home,
-      homePage: state.home.homePage
+        nav: state.navigation.nav,
+        title: state.title.title,
     }
-  }
+}
+
+  export default connect(mapStateToProps)(Skills)
