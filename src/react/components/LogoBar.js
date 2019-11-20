7a7d58cb4fe0488e7950a149b030c2449173d573
Robert Bergers <bergersr@my.easternct.edu>
2019-11-20 16:05:16 -0500

    tweak logobar

--- src/react/components/LogoBar.js
+++ src/react/components/LogoBar.js
@@ -4,13 +4,19 @@ const LogoBar = () => (
     <div id="logo-bar">
         <ul>
             <li>
-                <a href="https://www.github.com/robbyb97">
+                <a href="https://www.github.com/robbyb97" target="_blank">
                     <img src="/assets/images/icons/github.svg" />
+                    <p>
+                        GitHub
+                    </p>
                 </a>
             </li>
             <li>
-                <a href="https://www.linkedin.com/in/robbyb97">
+                <a href="https://www.linkedin.com/in/robbyb97" target="_blank">
                     <img src="/assets/images/icons/linkedin.png" />
+                    <p>
+                        LinkedIn
+                    </p>
                 </a>
             </li>
         </ul>
