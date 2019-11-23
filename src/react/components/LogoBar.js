f92890c122b9eced2f1776884b9bc69964c8f179
Robert Bergers <bergersr@my.easternct.edu>
2019-11-23 17:33:53 -0500

    remove text from logo bar

--- src/react/components/LogoBar.js
+++ src/react/components/LogoBar.js
@@ -6,17 +6,12 @@ const LogoBar = () => (
             <li>
                 <a href="https://www.github.com/robbyb97" target="_blank">
                     <img src="/assets/images/icons/github.svg" />
-                    <p>
-                        GitHub
-                    </p>
                 </a>
             </li>
+            
             <li>
                 <a href="https://www.linkedin.com/in/robbyb97" target="_blank">
                     <img src="/assets/images/icons/linkedin.png" />
-                    <p>
-                        LinkedIn
-                    </p>
                 </a>
             </li>
         </ul>
