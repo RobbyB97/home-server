154cdca104320ed9cb418b6a79980a970fb26a8e
Robert Bergers <me@robertbergers.com>
2020-01-09 10:39:16 -0800

    refactor: use CSSExtract

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,50 @@
 // Libraries
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 
-
-module.exports = {
-  entry: './src/react/app.js',
-  output: {
-    path: path.join(__dirname, 'public/'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ]
-    }]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public/react'),
-    historyApiFallback: true
-  }
+module.exports = () => {
+    const CSSExtract = new ExtractTextPlugin('bundle.css')
+    
+    return {
+        entry: './src/react/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    }
 }
