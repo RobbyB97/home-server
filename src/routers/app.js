e3bfaca652300217473a1dafb08e343533b8037d
Robert Bergers <me@robertbergers.com>
2020-01-09 10:40:54 -0800

    refactor routes

--- src/routers/app.js
+++ src/routers/app.js
@@ -7,28 +7,75 @@ const chalk = require('chalk')
 // Config
 const router = new express.Router()
 const http2https = require('../middleware/http2https')
+const publicPath = path.join(__dirname, '../../public/')
 
 
-// Homepage
-router.get('', http2https, (req, res) => {
-	try {
-        return res.return('/index')
+// Routes
+router.get('/', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+
     } catch(e) {
         res.send()
-        console.log(chalk.red('An error occured on /react: '))
+        console.log(chalk.red('An error occured on /: '))
         console.log(e)
     }
 })
 
-// Portfolio
-router.get('/portfolio', http2https, (req, res) => {
-  	try {
-    	return res.return('portfolio/index')
-  	} catch(e) {
-		res.send()
-		console.log(chalk.red('An error occured on /portfolio: '))
+router.get('/style_guide', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+        
+    } catch(e) {
+        res.send()
+        console.log(chalk.red('An error occured on /style_guide: '))
         console.log(e)
-  	}
+    }
 })
+
+router.get('/skills', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+        
+    } catch(e) {
+        res.send()
+        console.log(chalk.red('An error occured on /skills: '))
+        console.log(e)
+    }
+})
+
+router.get('/experience', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+    
+    } catch(e) {
+        res.send()
+        console.log(chalk.red('An error occured on /experience: '))
+        console.log(e)
+    }
+})
+
+router.get('/contact', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+
+    } catch(e) {
+        res.send()
+        console.log(chalk.red('An error occured on /contact: '))
+        console.log(e)
+    }
+})
+
+router.get('/projects', http2https, (req, res) => {
+    try {
+        return res.sendFile(path.join(publicPath, 'index.html'))
+
+    } catch(e) {
+        res.send()
+        console.log(chalk.red('An error occured on /projects: '))
+        console.log(e)
+    }
+})
+
 
 module.exports = router
