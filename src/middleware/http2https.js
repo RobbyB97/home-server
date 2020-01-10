bb3a557ee001e904a93b7650b16a7ca2df699a38
Robert Bergers <me@robertbergers.com>
2020-01-10 10:33:24 -0800

    spacing

--- src/middleware/http2https.js
+++ src/middleware/http2https.js
@@ -11,10 +11,12 @@ const http2https = (req, res, next) => {
   if (dev == 1) {
     console.log(chalk.blue('Skipping secure redirect on dev environment'))
     return next()
+
   } else {
     if(req.secure){
       return next()
-    };
+    }
+
     console.log(chalk.blue('Redirecting to https...'))
     res.redirect('https://' + req.hostname + req.url)
   }
