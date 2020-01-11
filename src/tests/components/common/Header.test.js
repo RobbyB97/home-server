57d27edec537dcefd679b385573b35a7b0e48005
Robert Bergers <bergersr@my.easternct.edu>
2020-01-10 19:46:44 -0500

    Header function tests

--- src/tests/components/common/Header.test.js
+++ src/tests/components/common/Header.test.js
@@ -4,13 +4,35 @@ import {shallow} from 'enzyme'
 import {Header} from '../../../react/components/common/Header'
 
 
-let wrapper
+let wrapper, goHome, toggleNav
 
 beforeEach(() => {
-    wrapper = shallow(<Header />)
+    goHome = jest.fn()
+    toggleNav = jest.fn()
+
+    wrapper = shallow(
+        <Header 
+            goHome={goHome}
+            toggleNav={toggleNav}
+        />
+    )
 })
 
 
 test('Render Header', () => {
     expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Navigation button toggle', () => {
+    wrapper.find('.header__nav-button').simulate('click')
+
+    expect(toggleNav).toHaveBeenCalled()
+})
+
+
+test('Home button', () => {
+    wrapper.find('NavLink').simulate('click')
+
+    expect(goHome).toHaveBeenCalled()
 })
