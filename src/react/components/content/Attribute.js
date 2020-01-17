edf7f735c220e1491bf87f48281d3e1c9554c9d9
Robert Bergers <me@robertbergers.com>
2020-01-17 06:28:28 -0800

    Refactor: convert Attribute to class component

--- src/react/components/content/Attribute.js
+++ src/react/components/content/Attribute.js
@@ -1,30 +1,30 @@
 import React from 'react'
 import {Link, NavLink} from 'react-router-dom'
-import {connect} from 'react-redux'
 
 
-const Attribute = (props) => (
-    <div className="attribute">
-        {
-            props.image &&
-                <div className="attribute__image">
-                    <img src={props.image}/>
-                </div>
-        }
-        {
-            props.title && 
-                <h2 className="attribute__title">
-                    {props.title}
-                </h2>
-        }
+export default class Attribute extends React.Component {
 
-        {   props.text &&
-                <p className="attribute__text">
-                    {props.text}
-                </p>
-        }
-
-    </div>
-)
-
-export default Attribute
+    render() {
+        return (
+            <div className={this.props.title ? `attribute ${this.props.title.toLowerCase()}` : "attribute"}>
+                {this.props.image &&
+                    <div className={this.props.fadeInTitle ? "attribute__image active" : "attribute__image"}>
+                        <img src={this.props.image}/>
+                    </div>
+                }
+                        
+                {this.props.title && 
+                    <h2 className={this.props.fadeInTitle === true ? "attribute__title active" : "attribute__title"}>
+                        {this.props.title}
+                    </h2>
+                }
+        
+                {this.props.text &&
+                    <p className={this.props.fadeInText === true ? "attribute__text active" : "attribute__text"}>
+                        {this.props.text}
+                    </p>
+                }
+            </div>
+        )
+    }
+}
