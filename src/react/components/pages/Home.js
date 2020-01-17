df49a632f95ad343daa2b5381da9689cd1202250
Robert Bergers <me@robertbergers.com>
2020-01-17 06:28:12 -0800

    Add content to home attributes

--- src/react/components/pages/Home.js
+++ src/react/components/pages/Home.js
@@ -7,6 +7,31 @@ import Attribute from '../content/Attribute'
 export class Home extends React.Component {
 
     render() {
+
+        const fastAttribute = {
+            image: "/dist/images/icons/speed_icon.png",
+            title: "Fast",
+            text: "Fully minified files and an optimized structure guarantee your website will load quickly, even on inconsistent internet connections."
+        }
+
+        const responsiveAttribute = {
+            image: "/dist/images/icons/responsive_icon.png",
+            title: "Responsive",
+            text: "Mobile first development built up to 4k screens guarantees that your website will be elegant and functional on any device."
+        }
+
+        const beautifulAttribute = {
+            image: "/dist/images/icons/paint_icon.png",
+            title: "Beautiful",
+            text: "A thorough, UX focused design process and open communication guarantees your website will look as beautiful as you envision it."
+        }
+
+        const accessibleAttribute = {
+            image: "/dist/images/icons/accessibility_icon.png",
+            title: "Accessible",
+            text: "Full compliance with ADA standards guarantees that your website will be accessible and easy to use for everyone."
+        }
+        
         return (
             <div id="home">
                 
@@ -17,26 +42,12 @@ export class Home extends React.Component {
 
                 {!this.props.splash &&
                     <div className="home__attributes">
-                        <Attribute 
-                            image="/dist/images/icons/speed_icon.png"
-                            title="Fast"
-                        />
-
-                        <Attribute 
-                            image="/dist/images/icons/responsive_icon.png"
-                            title="Responsive"
-                        />
-
-                        <Attribute
-                            image="/dist/images/icons/paint_icon.png"
-                            title="Beautiful"
-                        />
-
-                        <Attribute 
-                            image="/dist/images/icons/accessibility_icon.png"
-                            title="Accessible"
-                        />
-                    </div>}
+                        <Attribute {...fastAttribute} />
+                        <Attribute {...responsiveAttribute} />
+                        <Attribute {...beautifulAttribute} />
+                        <Attribute {...accessibleAttribute} />
+                    </div>
+                }
                 
             </div>
         )
