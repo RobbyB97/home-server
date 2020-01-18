2902f1da5d3b20fd46dfc5f882e0e92367e16617
Robert Bergers <me@robertbergers.com>
2020-01-18 07:51:30 -0800

    Restructure Attribute component

--- src/react/components/content/Attribute.js
+++ src/react/components/content/Attribute.js
@@ -7,23 +7,27 @@ export default class Attribute extends React.Component {
     render() {
         return (
             <div className={this.props.title ? `attribute ${this.props.title.toLowerCase()}` : "attribute"}>
-                {this.props.image &&
-                    <div className={this.props.fadeInTitle ? "attribute__image active" : "attribute__image"}>
-                        <img src={this.props.image}/>
-                    </div>
-                }
-                        
-                {this.props.title && 
-                    <h2 className={this.props.fadeInTitle === true ? "attribute__title active" : "attribute__title"}>
-                        {this.props.title}
-                    </h2>
-                }
-        
-                {this.props.text &&
-                    <p className={this.props.fadeInText === true ? "attribute__text active" : "attribute__text"}>
-                        {this.props.text}
-                    </p>
-                }
+                <div className="attribute__top">
+                    {this.props.image &&
+                        <div className={this.props.fadeInTitle ? "attribute__image active" : "attribute__image"}>
+                            <img src={this.props.image}/>
+                        </div>
+                    }
+                            
+                    {this.props.title && 
+                        <h2 className={this.props.fadeInTitle === true ? "attribute__title active" : "attribute__title"}>
+                            {this.props.title}
+                        </h2>
+                    }
+                </div>
+
+                <div className="attribute__bottom">
+                    {this.props.text &&
+                        <p className={this.props.fadeInText === true ? "attribute__text active" : "attribute__text"}>
+                            {this.props.text}
+                        </p>
+                    }
+                </div>
             </div>
         )
     }
