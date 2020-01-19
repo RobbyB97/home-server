06c0f5bfffede617edb5c5e58d01859903bad483
Robert Bergers <me@robertbergers.com>
2020-01-19 07:17:00 -0800

    Remove Attribute conditional classes, adjust design

--- src/react/components/content/Attribute.js
+++ src/react/components/content/Attribute.js
@@ -9,21 +9,21 @@ export default class Attribute extends React.Component {
             <div className={this.props.title ? `attribute ${this.props.title.toLowerCase()}` : "attribute"}>
                 <div className="attribute__top">
                     {this.props.image &&
-                        <div className={this.props.fadeInTitle ? "attribute__image active" : "attribute__image"}>
+                        <div className="attribute__image">
                             <img src={this.props.image}/>
                         </div>
                     }
                             
                     {this.props.title && 
-                        <h2 className={this.props.fadeInTitle === true ? "attribute__title active" : "attribute__title"}>
+                        <p className="attribute__title">
                             {this.props.title}
-                        </h2>
+                        </p>
                     }
                 </div>
 
                 <div className="attribute__bottom">
                     {this.props.text &&
-                        <p className={this.props.fadeInText === true ? "attribute__text active" : "attribute__text"}>
+                        <p className="attribute__text">
                             {this.props.text}
                         </p>
                     }
