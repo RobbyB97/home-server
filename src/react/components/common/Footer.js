f8cf4c9836eee958485b5ee5fb6e60742f47b4a5
Robert Bergers <me@robertbergers.com>
2020-01-20 11:29:07 -0800

    add structure to footer

--- src/react/components/common/Footer.js
+++ src/react/components/common/Footer.js
@@ -6,7 +6,12 @@ export class Footer extends React.Component {
     render() {
         return (
             <div id="footer">
+                <div class="footer__logos">
 
+                </div>
+                <div class="footer__copyright">
+                    
+                </div>
             </div>
         )
     }
