41cb8942f9ff31fda1da289701d7844001118d4c
Robert Bergers <bergersr@my.easternct.edu>
2020-01-23 17:42:11 -0500

    add test cases for Skill component

--- src/tests/components/content/Skill.test.js
+++ src/tests/components/content/Skill.test.js
@@ -2,10 +2,32 @@ import React from 'react'
 import {shallow} from 'enzyme'
 
 import Skill from '../../../react/components/content/Skill'
+import skills from '../../fixtures/skills'
 
 
 test('Render Skill with only a title', () => {
-    const wrapper = shallow(<Skill title="Title!"/>)
+    const wrapper = shallow(<Skill {...skills[0]}/>)
+
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render Skill with title and bits', () => {
+    const wrapper = shallow(<Skill {...skills[1]}/>)
+
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render Skill with title and description', () => {
+    const wrapper = shallow(<Skill {...skills[2]}/>)
+
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render Skill with title, bits and description', () => {
+    const wrapper = shallow(<Skill {...skills[3]}/>)
 
     expect(wrapper).toMatchSnapshot()
 })
---
+++ src/tests/fixtures/skills.js
@@ -0,0 +1,21 @@
+export default [{
+    title: "Title!"
+}, {
+    title: "Title!",
+    bits: [
+        "bit1",
+        "bit2",
+        "bit3"
+    ]
+}, {
+    title: "Title!",
+    description: "Description!"
+}, {
+    title: "Title!",
+    bits: [
+        "bit1",
+        "bit2",
+        "bit3"
+    ],
+    description: "Description!"
+}]
