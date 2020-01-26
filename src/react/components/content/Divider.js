62d7696068136f6d997cbe35542ade2db5bd8663
Robert Bergers <bergersr@my.easternct.edu>
2020-01-26 16:48:25 -0500

    Divider component initial commit

---
+++ src/react/components/content/Divider.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export class Divider extends React.Component {
+
+    render() {
+        return (
+            <div className="divider">
+
+            </div>
+        )
+    }
+}
+
+
+export default Divider
