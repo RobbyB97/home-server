ffabbd6c0cc463663f16a1ba82995a884a802a27
Robert Bergers <bergersr@my.easternct.edu>
2020-01-29 13:24:32 -0500

    Update Job component structure

--- src/react/components/content/Job.js
+++ src/react/components/content/Job.js
@@ -3,22 +3,65 @@ import React from 'react'
 
 export class Job extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            active: false
+        }
+    }
+
+    toggleOn = () => this.setState({active: true})
+    toggleOff = () => this.setState({active: false})
+
     render() {
         return (
-            <div className="job">
-                <p className="job__title">
-                    {this.props.title}
-                </p>
+            <div 
+                className={this.state.active ?
+                    "job active" :
+                    "job"}
+                onMouseEnter={this.toggleOn}
+                onMouseLeave={this.toggleOff}
+            >
+                {this.state.active ?
+                    
 
-                {this.props.text &&
-                    <p className="job__text">
-                        {this.props.text}
-                    </p>
-                }
+                    <div className="job__active">
+                        {this.props.time &&
+                            <div className="job__time">
+                                <p>
+                                    {this.props.time.start} - {this.props.time.end}
+                                </p>
+                            </div>
+                        }
 
-                {this.props.image &&
-                    <div className="job__image">
-                        <img src={this.props.image} />
+                        {this.props.link &&
+                            <div className="job__link">
+                                <a href={this.props.link}>
+                                    {this.props.name}'s website
+                                </a>
+                            </div>
+                        }
+
+                        <div className="job__description">
+                            <p>
+                                {this.props.description}
+                            </p>
+                        </div>
+                    </div>
+
+                :
+                    <div className="job__inactive">
+                        <div className="job__logo">
+                            {this.props.logo &&
+                                <img src={this.props.logo}/>
+                            }
+                        </div>
+
+                        <div className="job__name">
+                            <p>
+                                {this.props.name}
+                            </p>
+                        </div>
                     </div>
                 }
             </div>
--- src/tests/components/content/Job.test.js
+++ src/tests/components/content/Job.test.js
@@ -5,15 +5,8 @@ import {Job} from '../../../react/components/content/Job'
 import jobs from '../../fixtures/jobs'
 
 
-test('Render Job with only title', () => {
-    const wrapper = shallow(<Job {...jobs[0]}/>)
-
-    expect(wrapper).toMatchSnapshot()
-})
-
-
-test('Render Job with all props', () => {
-    const wrapper = shallow(<Job {...jobs[1]}/>)
+test('Render inactive Job with no props', () => {
+    const wrapper = shallow(<Job/>)
 
     expect(wrapper).toMatchSnapshot()
 })
