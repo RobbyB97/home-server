459197c1d4894f9f7d77987f38969b5b6c4a17b7
Robert Bergers <bergersr@my.easternct.edu>
2020-02-23 12:06:32 -0500

    fix http2https

--- src/middleware/http2https.js
+++ src/middleware/http2https.js
@@ -8,18 +8,8 @@ const dev = !process.env.PORT
 
 // Ensure secure connection
 const http2https = (req, res, next) => {
-  if (dev == 1) {
     console.log(chalk.blue('Skipping secure redirect on dev environment'))
     return next()
-
-  } else {
-    if(req.secure){
-      return next()
-    }
-
-    console.log(chalk.blue('Redirecting to https...'))
-    res.redirect('https://' + req.hostname + req.url)
-  }
 }
 
 module.exports = http2https
