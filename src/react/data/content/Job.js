89ed84de71d8d829fa3f793aabadb44a225051fe
Robert Bergers <bergersr@my.easternct.edu>
2020-03-01 16:28:02 -0500

    Job content objects initial commit

---
+++ src/react/data/content/Job.js
@@ -0,0 +1,23 @@
+export const job_finalsite = {
+    name: "Finalsite",
+    logo: "",
+    link: "",
+    position: "",
+    time: {
+        start: "",
+        end: ""
+    },
+    description: ""
+}
+
+export const job_twdh = {
+    name: "The Web Design Hub",
+    logo: "",
+    link: "",
+    position: "",
+    time: {
+        start: "",
+        end: ""
+    },
+    description: ""
+}
