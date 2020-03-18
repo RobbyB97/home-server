4430a2ef5f1e7b05992609e34b8300445a283332
Robert Bergers <bergersr@my.easternct.edu>
2020-03-18 13:11:00 -0400

    add HomeLink component structure

--- src/react/components/content/HomeLink.js
+++ src/react/components/content/HomeLink.js
@@ -6,8 +6,36 @@ export default class HomeLink extends React.Component {
 
     render() {
         return (
-            <div className="home-link">
-                HomeLink component
+            <div className='home-link'>
+                {this.props.external ?
+                    <a className="home-link__link" href={this.props.to}>
+                        {this.props.icon &&
+                            <img className="home-link__icon" src={this.props.icon} />
+                        }
+
+                        <p className="home-link__link-text">
+                            {this.props.linkText ?
+                                this.props.linkText
+                            :
+                                <p>HomeLink External</p>
+                            }
+                        </p>
+                    </a>
+                :
+                    <NavLink className="home-link__link" to={this.props.to}>
+                        {this.props.icon &&
+                            <img className="home-link__icon" src={this.props.icon} />
+                        }
+
+                        <p className='home-link__link-text'>
+                            {this.props.linkText ?
+                                this.props.linkText
+                            :
+                                <p>HomeLink Internal</p>
+                            }
+                        </p>
+                    </NavLink>
+                }
             </div>
         )
     }
---
+++ src/tests/fixtures/homelinks.js
@@ -0,0 +1,13 @@
+export default [{
+    icon: "icon!"
+}, {
+    linkText: "LinkText!"
+}, {
+    external: true
+}, {
+    external: true,
+    icon: "icon!"
+}, {
+    external: true,
+    linkText: "LinkText!"
+}]
