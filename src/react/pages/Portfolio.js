24e562660c22c346e2ad662777939fecf88648f9
Robert Bergers <me@robertbergers.com>
2020-04-01 10:35:49 -0700

    Portfolio page component

---
+++ src/react/pages/Portfolio.js
@@ -0,0 +1,32 @@
+/* Libraries */
+import React from 'react'
+
+
+/* Component */
+export class Portfolio extends React.Component {
+
+    render() {
+        return (
+            <div id="#Portfolio">
+                <div className="portfolio__about">
+
+                </div>
+                <div className="portfolio__services">
+                    
+                </div>
+                <div className="portfolio__skills">
+
+                </div>
+                <div className="portfolio__experience">
+
+                </div>
+                <div className="portfolio__projects">
+
+                </div>
+            </div>
+        )
+    }
+}
+
+
+export default Portfolio
