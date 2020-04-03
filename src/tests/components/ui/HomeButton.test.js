ca10da371080d8361c9acda05dd930bd1e170c77
Robert Bergers <me@robertbergers.com>
2020-04-03 11:43:19 -0700

    add HomeButton internal

--- src/tests/components/ui/HomeButton.test.js
+++ src/tests/components/ui/HomeButton.test.js
@@ -4,7 +4,13 @@ import {shallow} from 'enzyme'
 import {HomeButton} from '../../../react/components/ui/HomeButton'
 
 
-test('Render HomeButton', () => {
+test('Render HomeButton (external)', () => {
     const wrapper = shallow(<HomeButton />)
+    expect(wrapper).toMatchSnapshot()
+})
+
+
+test('Render HomeButton (internal)', () => {
+    const wrapper = shallow(<HomeButton internal />)
     expect(wrapper).toMatchSnapshot()
 })
