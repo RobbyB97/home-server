e71b06bd66e2b104dabffe2aa42952ca957b9146
Robert Bergers <me@robertbergers.com>
2020-04-28 17:33:08 -0400

    Add content to About section

--- src/react/components/About.js
+++ src/react/components/About.js
@@ -4,19 +4,24 @@ import React from 'react';
 export class About extends React.Component {
     render() {
         return (
-            <div id="about" className="about">
-                {/* Background fanciness top */}
+            <section id="About" className="about">
                 <div className="about__shape one"></div>
                 <div className="about__shape two"></div>
 
                 <div className="about__content">
-                    <div className="about__bio">
+                    <article className="about__bio">
                         <p>
-                            Hi, my name's Robby. I'm a full-stack web developer and digital artist. I love clean, responsive and intuitive interfaces, and so do your clients.
+                            Hi, my name's Robby. <span className="about__bioTitle">I'm a full-stack web developer and freelance digital artist.</span>
+
+                            <br /><br />
+                            I put my interdisciplinary skillset to good use by designing websites that are as beautiful as they are functional.
+                            
+                            <br /><br />
+                            <img className="about__portrait" src="/dist/images/4klanding_001phone.png"></img>
                         </p>
-                    </div>
+                    </article>
                 </div>
-            </div>
+            </section>
         );
     }
 }
