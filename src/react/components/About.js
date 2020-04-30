6056ff6b427dce7713ff233fcf257d4689e7844c
Robert Bergers <me@robertbergers.com>
2020-04-30 08:08:10 -0400

    Update structure

--- src/react/components/About.js
+++ src/react/components/About.js
@@ -1,23 +1,27 @@
 import React from 'react';
 
+import Divider from './Divider'
+
 
 export class About extends React.Component {
     render() {
         return (
             <section id="About" className="about">
-                <div className="about__shape one"></div>
-                <div className="about__shape two"></div>
+                <Divider section="about"/>
 
                 <div className="about__content">
+                    <div className="about__portrait">
+                        <img src="/dist/images/aboutImage.png" />
+                    </div>
+
                     <article className="about__bio">
                         <p>
-                            Hi, my name's Robby. <span className="about__bioTitle">I'm a full-stack web developer and freelance digital artist.</span>
+                            Hi, my name's Robby. <span className="about__bioTitle">I'm a front-end web developer and freelance digital artist.</span>
 
                             <br /><br />
-                            I put my interdisciplinary skillset to good use by designing websites that are as beautiful as they are functional.
+                            I put my interdisciplinary skillset to good use by building websites that are as beautiful as they are functional.
+
                             
-                            <br /><br />
-                            <img className="about__portrait" src="/dist/images/4klanding_001phone.png"></img>
                         </p>
                     </article>
                 </div>
