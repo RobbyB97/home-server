13f5ddd5058ca58066fa5e6c071a482c14fbdf64
Robert Bergers <bergersr97@gmail.com>
2020-06-27 11:11:32 -0400

    Add Experience component content

--- src/react/components/sections/Experience.js
+++ src/react/components/sections/Experience.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
+import ExperienceSection from '../ExperienceSection';
+
 
 export class Experience extends React.Component {
     render() {
         return (
             <section id="Experience" className="experience">
-                
+                <ExperienceSection 
+                    company="Finalsite"
+                    jobTitle="Front End Developer"
+                    description="I resolved bugs and implemented design changes on 
+                    Finalsite's proprietary CMS through client submitted tickets.
+                    I also improved upon their user guide."
+                />
+
+                <ExperienceSection 
+                    company="Web Design Hub"
+                    jobTitle="Junior Web Developer"
+                    description="I resolved bugs and added features to PHP sites on 
+                    The Web Design Hub's proprietary CMS."
+                />
             </section>
         );
     }
