c8c69b1a0c54ce34f30254f1f2ed3c96c8ed0d80
Robert Bergers <bergersr97@gmail.com>
2020-07-01 17:34:10 -0400

    Add footer divider

--- src/tests/components/Divider.test.js
+++ src/tests/components/Divider.test.js
@@ -1,10 +1,10 @@
-import React from 'react'
-import { shallow } from 'enzyme'
+import React from 'react';
+import { shallow } from 'enzyme';
 
-import { Divider } from '../../react/components/Divider'
+import { Divider } from '../../react/components/Divider';
 
 
 test('Render Divider', () => {
-    const wrapper = shallow(<Divider />)
-    expect(wrapper).toMatchSnapshot()
-})
+    const wrapper = shallow(<Divider />);
+    expect(wrapper).toMatchSnapshot();
+});
