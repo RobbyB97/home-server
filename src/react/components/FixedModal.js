7e25d6475c8accb29b31f13a3d3af439b95c3a97
Robert Bergers <bergersr97@gmail.com>
2020-07-02 14:07:56 -0400

    add ToggleOff function to remove popup

--- src/react/components/FixedModal.js
+++ src/react/components/FixedModal.js
@@ -7,20 +7,37 @@ export class FixedModal extends React.Component {
         this.state = {
             active: false
         };
-    }
+    };
+
 
     componentDidMount() {
         window.addEventListener('scroll', this.toggle);
-    }
+    };
+
 
     toggle = () => {
         // Only show once per visit
         window.removeEventListener('scroll', this.toggle);
+        window.addEventListener('scroll', this.toggleOff);
 
         this.setState({
             active: !this.state.active
         });
-    }
+    };
+
+
+    toggleOff = () => {
+        // Remove when scrolled to the bottom of the page
+        if ((window.innerHeight + window.scrollY) >= (document.body.offsetHeight - 10)) {
+            window.removeEventListener('scroll', this.toggleOff);
+            setTimeout(() => {
+                this.setState({
+                    active: false
+                });    
+            }, 500);
+        }
+    };
+
     
     render() {
         return (
@@ -42,7 +59,7 @@ export class FixedModal extends React.Component {
                 </article>
             </aside>
         );
-    }
+    };
 }
 
 
